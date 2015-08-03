33b1ddb2c022db44f7b0d81c4675f50286f5e998
Oscar Berry <oscar@madokami.com>
2015-08-03 02:11:12 +0100

    config mostly

--- static/includes/settings.inc.php
+++ static/includes/settings.inc.php
@@ -4,15 +4,15 @@
 // Or using socket would be even faster then 127.0.0.1 :3
 define('POMF_DB_CONN', 'mysql:host=127.0.0.1;dbname=pomf');
 // MySQL user and password
-define('POMF_DB_USER', 'pomf');
-define('POMF_DB_PASS', '***');
+define('POMF_DB_USER', getenv('POMF_DB_USER'));
+define('POMF_DB_PASS', getenv('POMF_DB_PASS'));
 
 // Root location of files
-define('POMF_FILES_ROOT', '/mnt/disk1/pomf/files/');
+define('POMF_FILES_ROOT', '/home/pomf/data/');
 // Maximum number of iterations while generating a new name
 define('POMF_FILES_RETRIES', 15);
 // URL to prepend to output (include trailing slash)
-define('POMF_URL', 'http://a.pomf.se/');
+define('POMF_URL', 'https://madokami.com/');
 
 $doubledots = array_map('strrev', array(
 	'tar.gz',
